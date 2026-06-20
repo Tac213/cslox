06e1c4d5a5cb061173ee7202472b3bf5595b6861
Tac Uchiha <cookiezhx@163.com>
2026-06-20 15:49:02 +0800

    Basic interpreter framework.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.IO;
 
-namespace Lox
+namespace cslox
 {
     internal class Program
     {
@@ -9,9 +9,38 @@ namespace Lox
         {
             if (args.Length > 1)
             {
-                Console.WriteLine("Usage: cslox [script]");
+                Console.Error.WriteLine("Usage: cslox [script]");
                 Environment.Exit(64);
             }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string path)
+        {
+            var bytes = File.ReadAllBytes(path);
+            Run(System.Text.Encoding.Default.GetString(bytes));
+        }
+
+        private static void RunPrompt()
+        {
+            for (; ; )
+            {
+                Console.Write("> ");
+                string? line = Console.ReadLine();
+                if (line == null) break;
+                Run(line);
+            }
+        }
+
+        private static void Run(string path)
+        {
         }
     }
 }
