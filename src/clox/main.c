c0ea142629a1fa7ed804b9ae80543570e53ad7d7
Tac Uchiha <cookiezhx@163.com>
2026-07-11 15:47:36 +0800

    Chapter 15: A Virtual Machine.

--- src/clox/main.c
+++ src/clox/main.c
@@ -1,14 +1,26 @@
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
     writeConstant(&chunk, 1.2, 1);
+    writeConstant(&chunk, 3.4, 1);
+    writeChunk(&chunk, OP_ADD, 1);
+
+    writeConstant(&chunk, 5.6, 1);
+    writeChunk(&chunk, OP_DIVIDE, 1);
+
+    writeChunk(&chunk, OP_NEGATE, 1);
     writeChunk(&chunk, OP_RETURN, 2);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
---
+++ src/clox/vm.c
@@ -0,0 +1,98 @@
+#include "vm.h"
+#ifdef DEBUG_TRACE_EXECUTION
+#include "debug.h"
+#endif
+#include <stdbool.h>
+#include <stdio.h>
+
+static VM vm;
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)                                                          \
+    do {                                                                       \
+        double b = pop();                                                      \
+        double a = pop();                                                      \
+        push(a op b);                                                          \
+    } while (false)
+
+    for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+        fprintf(stdout, "          ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+            fprintf(stdout, "[ ");
+            printValue(stdout, *slot);
+            fprintf(stdout, " ]");
+        }
+        fprintf(stdout, "\n");
+        disassembleInstruction(vm.chunk, (uint32_t)(vm.ip - vm.chunk->code));
+#endif
+
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+        case OP_CONSTANT: {
+            Value constant = READ_CONSTANT();
+            push(constant);
+            break;
+        }
+        case OP_CONSTANT_LONG: {
+            uint32_t constantIndex =
+                (uint32_t)READ_BYTE() | ((uint32_t)READ_BYTE() << 8) |
+                ((uint32_t)READ_BYTE() << 16) | ((uint32_t)READ_BYTE() << 24);
+            Value constant = vm.chunk->constants.values[constantIndex];
+            push(constant);
+            break;
+        }
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
+        case OP_NEGATE:
+            push(-pop());
+            break;
+        case OP_RETURN: {
+            printValue(stdout, pop());
+            fprintf(stdout, "\n");
+            return INTERPRET_OK;
+        }
+        default: {
+            break;
+        }
+        }
+    }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+#undef BINARY_OP
+}
+
+static void resetStack() { vm.stackTop = vm.stack; }
+
+void initVM() { resetStack(); }
+
+void freeVM() {}
+
+InterpretResult interpret(Chunk *chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
